bcfd20907e72e20881d752512d3073b9c3a64573
timschottehogent <36566526+timschottehogent@users.noreply.github.com>
2018-07-30 16:42:46 +0200

    h5
    H5 halfweg

---
+++ fitnessapp/backend/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+
+
+let mongoose = require('mongoose');
+let Recipe = mongoose.model('Recipe');
+
+router.get('/API/recipes/', function(req, res, next) {
+  Recipe.find(function(err, recipes) {
+    if (err) { return next(err); }
+    res.json(recipes);
+  });
+});
+
+module.exports = router;
