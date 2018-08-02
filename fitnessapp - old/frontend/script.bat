7fb14ad4158f255a167376e7ca03ca98509760fa
timschottehogent <36566526+timschottehogent@users.noreply.github.com>
2018-08-02 17:08:47 +0200

    start
    Start from scratch

--- fitnessapp - old/frontend/script.bat
+++
@@ -1,3 +0,0 @@
-for i in `ls -d -A * | grep -v frontend`; 
-do git mv $i frontend/; done 
-git mv .angular-cli.json frontend/
--- fitnessapp - old/frontend/src/app/recipe-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RecipeFilterPipe } from './recipe-filter.pipe';
-
-describe('RecipeFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RecipeFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
